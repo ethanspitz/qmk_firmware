4ee091ae8f47d7bcda8537da07ae3f397320afc3
Ethan Spitz <ethan.spitz@gmail.com>
2019-07-13 02:30:45 -0700

    Upgrades!
    Fix Right hand Row 5
    Switch to Elite-C
    Make sidedness static by hardwired pin
    New keymap that is useable!

--- keyboards/handwired/dactyl_promicro/keymaps/ethan/config.h
+++ keyboards/handwired/dactyl_promicro/keymaps/ethan/config.h
@@ -21,5 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define MASTER_RIGHT
 //#define MASTER_RIGHT
-#define EE_HANDS
+#define SPLIT_HAND_PIN F4
+//#define EE_HANDS
 // Rows are doubled-up
